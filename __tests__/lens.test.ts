9790e430a5ed1eb523ef9e372ecd397043a16f93
clbrem <christopher.bremer@nov.com>
2023-01-03 12:32:59 -0600

    first commit

---
+++ __tests__/lens.test.ts
@@ -0,0 +1,168 @@
+import {
+  start,
+  field,
+  index,
+  withDefault,
+  pushPeek,
+  lens,
+  grouped,
+  toGrouped,
+  mergeApply,
+  setMany
+} from "../lens";
+
+type TestType = {
+  a: number;
+  b: string;
+  inner?: TestType;
+};
+type Container = {
+  contained: TestType;
+};
+type ArrayContainer = {
+  arrayed: number[];
+};
+
+const notTesting: TestType = {
+  a: 2,
+  b: "notTested"
+};
+const testing: TestType = {
+  a: 1,
+  b: "tested"
+};
+const nesting: TestType = {
+  a: 2,
+  b: "nested",
+  inner: testing
+};
+const containing = {
+  contained: testing
+};
+
+const array = [1, 2, 3, 4, 5];
+
+const arraying = {
+  arrayed: array
+};
+const a = field<TestType, "a">("a");
+const contained = field<Container, "contained">("contained");
+const id = index<number>(3);
+const outOfRange = index<number>(6);
+const arrayed = field<ArrayContainer, "arrayed">("arrayed");
+const defaulted = field<TestType, "inner">("inner").pipe(
+  withDefault<TestType>(testing)
+);
+
+test("Can extract field", () => {
+  expect(a.view(testing)).toBe(1);
+  expect(a.view(nesting)).toBe(2);
+  expect(contained.pipe(a).view(containing)).toBe(1);
+});
+test("Can insert field", () => {
+  let testingNew = a.set(testing, 3);
+  expect(a.view(testingNew)).toBe(3);
+  expect(a.view(testingNew)).not.toBe(a.view(testing));
+});
+test("Can Insert Element", () => {
+  let arrayNew = id.set(array, 2);
+  expect(arrayNew).toMatchObject([1, 2, 3, 2, 5]);
+  expect(array).toMatchObject([1, 2, 3, 4, 5]);
+});
+test("Can insert nested ", () => {
+  let newContaining = contained.pipe(a).set(containing, 3);
+  expect(contained.pipe(a).view(newContaining)).toBe(3);
+});
+
+test("Can extract item from array", () => {
+  expect(id.view(array)).toBe(4);
+  expect(arrayed.pipe(id).view(arraying)).toBe(4);
+});
+
+test("Extract Item Too far", () => {
+  expect(outOfRange.view(array)).toBeUndefined();
+  let inserted = outOfRange.set(array, 6);
+  expect(outOfRange.view(inserted)).toBe(6);
+  let reallyShort = [1, 2];
+  let insertedShort = outOfRange.set(reallyShort, 6);
+
+  expect(outOfRange.view(insertedShort)).toBe(6);
+  let almostLongEnough = [1, 2, 3, 4, 5, 6];
+  let insertedAlmost = outOfRange.set(almostLongEnough, 7);
+  expect(outOfRange.view(insertedAlmost)).toBe(7);
+});
+
+test("Testing Default", () => {
+  let newTesting = a.set(testing, 7);
+  let newNesting = defaulted.set(testing, newTesting);
+
+  expect(defaulted.view(testing)).toMatchObject(testing);
+  expect(defaulted.view(newNesting)).toMatchObject(newTesting);
+});
+
+test("Test pushPeek", () => {
+  let pp = pushPeek<number>();
+  expect(pp.view(array)).toBe(5);
+  expect(pp.view(pp.set(array, 6))).toBe(6);
+  expect(pp.view([])).toBeUndefined();
+  expect(pp.set([], 1)).toMatchObject([1]);
+});
+test("Test pipe", () => {
+  let l = start<TestType>().pipe(
+    field("inner"),
+    withDefault(notTesting),
+    field("a")
+  );
+  expect(l.view(nesting)).toBe(1);
+});
+
+function lensTestPrimitive<S, T>(
+  l: lens<S, T>,
+  store: S,
+  value: T,
+  secondValue: T
+) {
+  expect(l.view(l.set(store, value))).toBe(value);
+  expect(l.view(l.set(l.set(store, secondValue), value))).toBe(
+    l.view(l.set(store, value))
+  );
+  expect(l.view(l.set(store, l.view(store)))).toBe(l.view(store));
+}
+
+// function lensTestObject<S, T>(
+//   l: lens<S, T>,
+//   store: S,
+//   value: T,
+//   secondValue: T
+// ) {
+//   expect(l.view(l.set(store, value))).toMatchObject(value);
+//   expect(l.view(l.set(l.set(store, secondValue), value))).toMatchObject(
+//     l.view(l.set(store, value))
+//   );
+//   expect(l.view(l.set(store, l.view(store)))).toMatchObject(l.view(store));
+// }
+
+test("Test Lens Properties", () => {
+  lensTestPrimitive(pushPeek<number>(), [1, 2, 3], 1, 2);
+  lensTestPrimitive(pushPeek<number>(), [], 2, 1);
+});
+
+test("Test Grouped", () => {
+  let t = toGrouped((x: number) => `${x % 3}`)([1, 2, 3, 4, 5]);
+  expect(grouped("1").view(t)).toEqual(4);
+});
+test("Test MergeApply", () => {
+  let out = mergeApply<number, { arrayed: number[] }, number[]>(
+    pushPeek<number>().set,
+    index<number>(0).set,
+    (s, i) => index<number>(3).set(s, index<number>(3).view(s) / i)
+  )(field("arrayed"))(arraying, 2);
+  expect(out.arrayed).toMatchObject([2, 2, 3, 2, 5, 2]);
+});
+test("Test MergeApplyWithSetMany", () => {
+  let out = field<ArrayContainer, "arrayed">("arrayed").mergeApply(
+    pushPeek<number>().setMany<number>(i => i ** 2),
+    index<number>(0).setApply(item => item.reduce((acc, curr) => acc + curr))
+  )(arraying, [1, 2, 3, 4]);
+  expect(out.arrayed).toMatchObject([10, 2, 3, 4, 5, 1, 4, 9, 16]);
+});
---
+++ lens.ts
@@ -0,0 +1,325 @@
+export type lens<S, T> = {
+  view: (store: S) => T;
+  set: (store: S, value: T) => S;
+};
+
+export const setApply = <U, S, T>(fn: (item: U) => T) => (l: lens<S, T>) => (
+  store: S,
+  item: U
+) => l.set(store, fn(item));
+
+export const setMany = <U, S, T>(fn: (item: U, i: number, array: U[]) => T) => (
+  l: lens<S, T>
+) => (store: S, items: U[]) => items.map(fn).reduce(l.set, store);
+
+export const setConcat = <U, S, T>(
+  fn: (item: U, i: number, array: U[]) => T[]
+) => (l: lens<S, T>) => (store: S, items: U[]) =>
+  items
+    .map(fn)
+    .reduce((acc, curr) => acc.concat(curr), [])
+    .reduce(l.set, store);
+
+export const mergeApply = <U, S, T>(
+  fn: (store: T, item: U) => T,
+  ...fns: ((store: T, item: U) => T)[]
+) => (l: lens<S, T>) => (store: S, item: U) =>
+  fns.length > 0
+    ? l.set(
+        store,
+        fns.reduce((acc: T, curr) => curr(acc, item), fn(l.view(store), item))
+      )
+    : l.set(store, fn(l.view(store), item));
+
+export const forEach = <U, S, T>(fn: (item: T) => T, l: lens<S, T[]>) => (
+  store: S,
+  items: U[]
+) => items;
+
+const viewApply = <U, S, T>(l: lens<S, T>, fn: (item: T) => U) => (store: S) =>
+  fn(l.view(store));
+
+const map = <S, T>(fn: (n: T) => T, l: lens<S, T>) => (source: S): S =>
+  l.set(source, fn(l.view(source)));
+
+export const identity = <T>(): lens<T, T> => ({
+  view: s => s,
+  set: (_, t) => t
+});
+
+export function pipe<T>(): lens<T, T>;
+export function pipe<T, A>(l1: lens<T, A>): lens<T, A>;
+export function pipe<T, A, B>(l1: lens<T, A>, l2: lens<A, B>): lens<T, B>;
+export function pipe<T, A, B, C>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>
+): lens<T, C>;
+export function pipe<T, A, B, C, D>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>,
+  l4: lens<C, D>
+): lens<T, D>;
+export function pipe<T, A, B, C, D, E>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>,
+  l4: lens<C, D>,
+  l5: lens<D, E>
+): lens<T, E>;
+export function pipe<T, A, B, C, D, E, F>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>,
+  l4: lens<C, D>,
+  l5: lens<D, E>,
+  l6: lens<E, F>
+): lens<T, F>;
+export function pipe<T, A, B, C, D, E, F, G>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>,
+  l4: lens<C, D>,
+  l5: lens<D, E>,
+  l6: lens<E, F>,
+  l7: lens<F, G>
+): lens<T, G>;
+export function pipe<T, A, B, C, D, E, F, G, H>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>,
+  l4: lens<C, D>,
+  l5: lens<D, E>,
+  l6: lens<E, F>,
+  l7: lens<F, G>,
+  l8: lens<G, H>
+): lens<T, H>;
+export function pipe<T, A, B, C, D, E, F, G, H, I>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>,
+  l4: lens<C, D>,
+  l5: lens<D, E>,
+  l6: lens<E, F>,
+  l7: lens<F, G>,
+  l8: lens<G, H>,
+  l9: lens<H, I>
+): lens<T, I>;
+export function pipe<T, A, B, C, D, E, F, G, H, I>(
+  l1: lens<T, A>,
+  l2: lens<A, B>,
+  l3: lens<B, C>,
+  l4: lens<C, D>,
+  l5: lens<D, E>,
+  l6: lens<E, F>,
+  l7: lens<F, G>,
+  l8: lens<G, H>,
+  l9: lens<H, I>,
+  ...arr: lens<any, any>[]
+): lens<T, {}>;
+
+export function pipe(...fns: Array<lens<any, any>>): lens<any, any> {
+  return pipeFromArray(fns);
+}
+
+export class Lens<S, T> implements lens<S, T> {
+  constructor(
+    public view: (store: S) => T,
+    public set: (store: S, value: T) => S
+  ) {}
+  public static From<S, T>(lens: lens<S, T>) {
+    return new Lens<S, T>(lens.view, lens.set);
+  }
+  public setApply<U>(fn: (item: U) => T) {
+    return setApply<U, S, T>(fn)(this);
+  }
+  public viewApply<U>(fn: (item: T) => U) {
+    return viewApply(this, fn);
+  }
+  public setMany<U>(fn: (item: U, i?: number, arr?: U[]) => T) {
+    return setMany<U, S, T>(fn)(this);
+  }
+  public mergeApply<U>(
+    fn: (store: T, item: U) => T,
+    ...fns: ((store: T, item: U) => T)[]
+  ) {
+    return mergeApply<U, S, T>(fn, ...fns)(this);
+  }
+  public setConcat<U>(fn: (items: U, i?: number, arr?: U[]) => T[]) {
+    return setConcat<U, S, T>(fn)(this);
+  }
+  public map(fn: (item: T) => T) {
+    return map(fn, this);
+  }
+  public pipe(): Lens<S, T>;
+  public pipe<A>(lens: lens<T, A>): Lens<S, A>;
+  public pipe<A, B>(l1: lens<T, A>, l2: lens<A, B>): Lens<S, B>;
+  public pipe<A, B, C>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>
+  ): Lens<S, C>;
+  public pipe<A, B, C, D>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>,
+    l4: lens<C, D>
+  ): Lens<S, D>;
+  public pipe<A, B, C, D, E>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>,
+    l4: lens<C, D>,
+    l5: lens<D, E>
+  ): Lens<S, E>;
+  public pipe<A, B, C, D, E, F>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>,
+    l4: lens<C, D>,
+    l5: lens<D, E>,
+    l6: lens<E, F>
+  ): Lens<S, F>;
+  public pipe<A, B, C, D, E, F, G>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>,
+    l4: lens<C, D>,
+    l5: lens<D, E>,
+    l6: lens<E, F>,
+    l7: lens<F, G>
+  ): Lens<S, G>;
+  public pipe<A, B, C, D, E, F, G, H>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>,
+    l4: lens<C, D>,
+    l5: lens<D, E>,
+    l6: lens<E, F>,
+    l7: lens<F, G>,
+    l8: lens<G, H>
+  ): Lens<S, H>;
+  public pipe<A, B, C, D, E, F, G, H, I>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>,
+    l4: lens<C, D>,
+    l5: lens<D, E>,
+    l6: lens<E, F>,
+    l7: lens<F, G>,
+    l8: lens<G, H>,
+    l9: lens<H, I>
+  ): Lens<S, I>;
+  public pipe<A, B, C, D, E, F, G, H, I>(
+    l1: lens<T, A>,
+    l2: lens<A, B>,
+    l3: lens<B, C>,
+    l4: lens<C, D>,
+    l5: lens<D, E>,
+    l6: lens<E, F>,
+    l7: lens<F, G>,
+    l8: lens<G, H>,
+    l9: lens<H, I>,
+    ...arr: lens<any, any>[]
+  ): Lens<S, {}>;
+
+  public pipe(...l: lens<any, any>[]): Lens<any, any> {
+    let inner = pipeFromArray([this, ...l]);
+    return new Lens(inner.view, inner.set);
+  }
+}
+
+export const field = <S extends object, K extends keyof S>(
+  name: K
+): Lens<S, S[K]> =>
+  new Lens(
+    (store: S) => store[name],
+    (store: S, value: S[K]) => ({ ...store, [name]: value } as S)
+  );
+
+export const index = <S>(id: number): Lens<S[], S> =>
+  new Lens(
+    (item: S[]) => item[id],
+    (store: S[], value: S) =>
+      store
+        .slice(0, id)
+        .concat(new Array(Math.max(0, id - store.length)))
+        .concat([value])
+        .concat(store.slice(id + 1))
+  );
+
+export const withDefault = <T>(def: T): Lens<T | undefined, T> =>
+  new Lens(store => store || def, (_, value) => value);
+
+export const maybeArray = <T>(): Lens<T[] | T | undefined, T[]> =>
+  new Lens(
+    store =>
+      typeof store === "undefined"
+        ? []
+        : Array.isArray(store)
+        ? store
+        : [store],
+    (_, value) => value
+  );
+export const pushPeek = <T>(): Lens<T[], T | undefined> =>
+  new Lens(
+    store => store.slice(-1).pop(),
+    (store, value) => (value ? [...store, value] : store)
+  );
+
+export const start = <T>(): Lens<T, T> =>
+  new Lens(store => store, (_, value) => value);
+
+export const from = <S, T>(
+  getter: (store: S) => T,
+  setter: (store: S, value: T) => S
+) => new Lens<S, T>(getter, setter);
+
+function pipeInner<S, T, U>(first: lens<S, T>, second: lens<T, U>): lens<S, U> {
+  return {
+    view: (store: S) => second.view(first.view(store)),
+    set: (store: S, value: U) =>
+      first.set(store, second.set(first.view(store), value))
+  };
+}
+function pipeFromArray(fns: Array<lens<any, any>>): lens<any, any> {
+  if (!fns) {
+    return identity();
+  }
+  if (fns.length === 1) {
+    return fns[0];
+  }
+  return fns.reduce(pipeInner);
+}
+export function defaultArray<T>(): Lens<T[] | undefined, T[]> {
+  return withDefault(new Array<T>());
+}
+
+export const mapKey = <V>(
+  key: string
+): Lens<{ [key: string]: V }, V | undefined> =>
+  new Lens(
+    map => map[key],
+    (map, v: V | undefined) =>
+      typeof v === "undefined" ? map : { ...map, [key]: v }
+  );
+
+export const grouped = <T>(key: string) =>
+  mapKey<T[]>(key).pipe(
+    defaultArray(),
+    pushPeek()
+  );
+
+export const toGrouped = <T>(fn: (item: T) => string) => (items: T[]) =>
+  items.reduce((acc, item) => grouped<T>(fn(item)).set(acc, item), {} as {
+    [key: string]: T[];
+  });
+
+export const nil = <T>(): lens<T, T> => ({
+  view: s => undefined,
+  set: (s, _) => s
+});
+
+export const iff = <T>(statement: boolean): lens<T, T> =>
+  statement ? identity() : nil();
